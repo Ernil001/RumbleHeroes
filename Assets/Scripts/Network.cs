6de0bfc5b6657c9b59a63cca0bb4b57cd43a44a1
Jure Veler <jure.veler@gmail.com>
2015-04-11 21:57:47 +0200

    a

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -96,7 +96,11 @@ public class Network : MonoBehaviour
         roomsList = PhotonNetwork.GetRoomList();
 
         // Clear all rooms first
-        
+        /*
+        var children = new List<GameObject>();
+        foreach (Transform child in transform) children.Add(child.gameObject);
+        children.ForEach(child => Destroy(child));
+        */
 
         //Populate refresh
         if (roomsList != null)
