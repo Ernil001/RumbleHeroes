2dc00368cf17c65324dfa72adb111d387af79325
Jure Veler <jure.veler@gmail.com>
2015-05-21 19:11:27 +0200

    aaaa

--- Assets/Scripts/Game/Projectiles/ArrowShot.cs
+++ Assets/Scripts/Game/Projectiles/ArrowShot.cs
@@ -24,7 +24,7 @@ public class ArrowShot : Projectile
 
         base.Start();
     }
-    /*
+    
     protected override void Update()
     {
         //Debug.Log("ASD");
@@ -33,8 +33,8 @@ public class ArrowShot : Projectile
 
 
 
-        base.Start();
+        base.Update();
     }
-    */
+    
 }
 
