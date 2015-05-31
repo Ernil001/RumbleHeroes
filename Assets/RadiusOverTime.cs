cc80ec89716be38b5053c7e9b7e19bb444d47e51
Henric Johansson <henric.johansson88@gmail.com>
2015-05-31 09:59:05 +0200

    Added Repell spell, 80% complete

---
+++ Assets/RadiusOverTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadiusOverTime : MonoBehaviour {
+
+    public float startRadius;
+    public float endRadius;
+    public float overTime;
+
+    private float timeStarted;
+    private CircleCollider2D collider;
+
+	// Use this for initialization
+	void Start () {
+        this.timeStarted = Time.time;
+        collider = this.GetComponent<CircleCollider2D>();
+        collider.radius = startRadius;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time - this.timeStarted < this.overTime)
+        {
+            collider.radius = (Time.time - this.timeStarted) * endRadius;
+        }
+	}
+}
---
+++ Assets/Scripts/Game/SelfBuffs/Repell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Repell : SelfBuff
+{
+    protected override void Start()
+    {
+        if (ownerGameObject.photonView.isMine)
+        {
+            Physics2D.IgnoreCollision(ownerGameObject.GetComponent<BoxCollider2D>(), this.GetComponent<CircleCollider2D>());
+        }
+    }
+    /*
+	protected override void Update () 
+    {
+        base.Update();
+	}
+    */
+    public void OnDestroy()
+    {
+        //ownerGameObject.GetComponent<PlayerController>().speed = ownerGameObject.GetComponent<PlayerController>().speedBase;
+    }
+
+}
