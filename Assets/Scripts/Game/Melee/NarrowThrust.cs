a0f306b0bb7bdf0cb4e4153210fc9e7e09a78875
Jure Veler <jure.veler@gmail.com>
2015-06-01 20:51:02 +0200

    Finished Nejito First Attack +  Melee.cs is created

--- Assets/Scripts/Game/Melee/NarrowThrust.cs
+++ Assets/Scripts/Game/Melee/NarrowThrust.cs
@@ -5,6 +5,7 @@ public class NarrowThrust : Melee
 {
     //-***********
     //  Primary thrust of the ninja Nejito
+    //  Might have issues with staying too long on the field.
     //-***********
     //
     protected override void Start()
