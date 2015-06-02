02734349e1f360c2b557954abd2847cd1f4b3131
Jure Veler <jure.veler@gmail.com>
2015-06-02 14:12:16 +0200

    Worked a bit on UI+SoundEvents

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -10,6 +10,9 @@ public class AudioController : MonoBehaviour
     //*************
     public static AudioController instance = null;
 	//
+    public AudioClip UI_ButtonHover;
+    public AudioClip UI_ButtonClick;
+    //
     void Awake()
     {
         // Makes the current instance a static one.
@@ -20,7 +23,10 @@ public class AudioController : MonoBehaviour
     }
 	void Start () 
     {
-	
+	    // On start collect data from options on sound volume and set it apropriatelly for each subdivision of sound
+        OptionController.instance.MasterVolume = OptionController.instance.MasterVolume; //Looks weird but the set encapsulation sets the sound aswell, so it works :X.
+
+
 	}
 	void Update ()
     {
@@ -57,11 +63,25 @@ public class AudioController : MonoBehaviour
         Destroy(obj, file.length); 
         return aSource; 
     }
-    //
-    public void playClip_ability(AudioClip file, Vector3 pos, string objName = "Default:", object[] customPar = null)
+    // Different version for starting the same Method, playClipAtPoint;
+    public void playClip_ability(AudioClip file, Vector3 pos, string objName = "Ability:", object[] customPar = null)
     {
         //AudioSource.PlayClipAtPoint(file, pos);
         playClipAtPoint(file, pos, objName, customPar);
     }
+    public void playClip_default(AudioClip file, Vector3 pos, string objName = "Default:", object[] customPar = null)
+    {
+        playClipAtPoint(file, pos, objName, customPar);
+    }
+    // Main method for playing sound for UI; I might change this a bit, dont like it.
+    public void playClip_UI_ButtonClick()
+    {
+        AudioSource.PlayClipAtPoint(UI_ButtonClick, new Vector3(0, 0, 0));
+    }
+    public void playClip_UI_ButtonHover()
+    {
+        AudioSource.PlayClipAtPoint(UI_ButtonHover, new Vector3(0, 0, 0));
+    }
+
 
 }
